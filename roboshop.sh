28ecc595be62f8342e5d58bbb5d07a4a7dcf5e53
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-16 06:50:49 +0530

    updated .sh file

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-05847b88288dddafe" # replace with your SG ID
-INSTANCE=("mongodb" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z09266885JREMC64NO1J"
 DOMAIN_NAME="bharath2103.site"
 
